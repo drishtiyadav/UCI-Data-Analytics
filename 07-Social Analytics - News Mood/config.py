70f0af3a55b47ca791012c6a4303db1468cb82d0
Drishti Yadav <drishti.y@gmail.com>
2018-09-17 00:07:13 -0700

    9/17 twitter assignment first draft

---
+++ 07-Social Analytics - News Mood/config.py
@@ -0,0 +1,5 @@
+#Twitter API keys
+consumer_key = "HBxotIMdrKx1wtPr0K3iMuRgT"
+consumer_secret = "B6OLRt49t8TSDa93QBAyElZy7mX3V50dY6qiC9PDoVmVSnKZcy"
+access_token = "1032002074664288256-6d9E49F3D4SsluPkNdhZONyb6xYTYB"
+access_token_secret = "nmbm45AUDxsrG01YZmIMhg6w4EjgpIMgeE4LuyILxOuHk"
